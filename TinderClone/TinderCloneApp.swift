c77b00f633fbe97d39c6f8360c1830de9fbaddbf
GosutoDev <duchoslav.tomas@gmail.com>
2024-04-02 13:16:24 +0200

    Initial Commit

---
+++ TinderClone/TinderCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderCloneApp.swift
+//  TinderClone
+//
+//  Created by Tomáš Duchoslav on 02.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct TinderCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
